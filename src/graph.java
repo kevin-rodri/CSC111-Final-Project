dddfaff730533a353226b97056e0953b81fec551
GandhiHarsh2003 <23gandhiharsh@gmail.com>
2022-05-02 16:58:24 -0400

    new file

---
+++ src/graph.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+
+import org.math.plot.Plot2DPanel;
+
+public class graph {
+	public static void main(String[] args) {
+		// create your PlotPanel (you can use it as a JPanel)
+		Plot2DPanel plot = new plot2DPlanel();
+		File file = new File("./grades.txt");
+
+		
+	}
+}
